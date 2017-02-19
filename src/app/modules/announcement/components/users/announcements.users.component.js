997db4128531a0eb664fd89d0e499da835c4592a
Leonan Teixeira <leonan.teixeira@gmail.com>
2017-02-19 07:40:52 -0300

    Announcement users

--- src/app/modules/announcement/components/users/announcements.users.component.js
+++ src/app/modules/announcement/components/users/announcements.users.component.js
@@ -4,24 +4,20 @@ export const AnnouncementUsersComponent  = {
     template: require('./announcements.users.component.html'),
     controllerAs: 'vm',
     controller: class AnnouncementUsersComponent {
-        constructor(AnnouncementService) {
+        constructor(AnnouncementService, $stateParams, $scope) {
             'ngInject';
             this.announcementService = AnnouncementService;
+            this.$stateParams = $stateParams;
+            this.$scope = $scope;
         }
 
         $onInit() {
-            this.chatUsers = [{
-                image: 'https://s-media-cache-ak0.pinimg.com/736x/20/45/08/2045085b168394d854b2477b3fbca751.jpg',
-                name: 'Peppo Pig'
-            },
-            {
-                image: 'http://vignette2.wikia.nocookie.net/parody/images/9/9a/STASC545-Peppa-Pig-Candy-Cat-Cutout_3.png/revision/latest?cb=20140423150749',
-                name: 'Candy Cat'
-            },
-            {
-                image: 'http://orig03.deviantart.net/f3d1/f/2015/078/0/c/adventure_time_jake___animation__by_demoniumangel-d8mcmsm.gif',
-                name: 'Jake'
-            }];
+            this.users = [];
+            this.announcementService.getUsers(this.$stateParams.id, (user)=> {
+                this.$scope.$apply(()=>{
+                    this.users.push(user);
+                });
+            });
         }
     }
 };
